3076d3e81b9d457fc544ddbfcc07ee9ea5091e13
spottran2001 <spottran2001@gmail.com>
2022-07-13 10:41:10 +0700

    change db link

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.HOST || 3101;
 
-const URI = process.env.DB_URL;
+const URI = "mongodb+srv://admin:s3bq4UOIELOJPAOA@cluster0.nffpq.mongodb.net/?retryWrites=true&w=majority";
 
 app.use(bodyParser.json({ limit: '30mb'}));
 app.use(bodyParser.urlencoded({extended: true, limit: '30mb'}));
@@ -18,7 +18,6 @@ app.use(cors());
 
 app.use('/accounts', accounts);
 
-console.log(process.env.PORT);
 mongoose
     .connect(URI, { useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => {
