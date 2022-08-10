e379159f7cb8afce6df48d491d706787974fa91b
spottran2001 <spottran2001@gmail.com>
2022-08-10 16:52:06 +0700

    fix cors

--- index.js
+++ index.js
@@ -22,11 +22,12 @@ const PORT = process.env.PORT || 3101;
 
 const URI = process.env.MONGO_DB;
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+app.use(cors());
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
 
 app.use(bodyParser.json({ limit: '30mb'}));
 app.use(bodyParser.urlencoded({extended: true, limit: '30mb'}));
