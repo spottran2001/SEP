cede390bf2d41500f677acf04e9efb0bfec9d7b7
spottran2001 <spottran2001@gmail.com>
2022-08-11 15:03:31 +0700

    clean code

--- controllers/statistics.js
+++ controllers/statistics.js
@@ -5,85 +5,51 @@ export const statistics = async (req, res) => {
     try {
         const {month, year} = req.body;
 
-        var datetime = new Date(year, month, 1);
-        datetime.setUTCHours(0,0,0,0);
+        // var datetime = new Date(year, month, 1);
+        // datetime.setUTCHours(0,0,0,0);
 
-        var lastDayOfMonth = datetime.toISOString();
+        // var lastDayOfMonth = datetime.toISOString();
 
-        datetime.setDate(datetime.getDate() -60)
-        var firstDayOfMonth =datetime.toISOString();
+        // var datetime1 = new Date(2022, 6, 2);
+        // datetime1.setUTCHours(0,0,0,0);
+        // //datetime.setDate(datetime.getDate() -60)
+        // var firstDayOfMonth =datetime1.toISOString();
 
-        const days_data = {
-            firstDayOfMonth,
-            lastDayOfMonth
-        }
+        // const days_data = {
+        //     firstDayOfMonth,
+        //     lastDayOfMonth
+        // }
 
-        console.log(days_data);
-
-        // const sum_data = await DishModel.aggregate([
-        //                     { $match: {
-        //                         createdAt: {
-        //                             $gte: ISODate(firstDayOfMonth),
-        //                             $lte: ISODate(lastDayOfMonth)
-        //                             }
-        //                         }
-        //                     },
-        //                     {
-        //                         $group: {
-        //                         _id: null,
-        //                         totalQty: { $sum: '$price' },
-        //                         },
-        //                     },
-                            
-        //                 ]);
-        
-        // const sum_data = await DishModel.find({$expr:{$eq: [{$month:"$createdAt", month }]}}     
+        // console.log(days_data);
+        // const sum_data = await BankModel.find({
+        //                                     createdAt: {
+        //                                         $gte: firstDayOfMonth,
+        //                                         $lte: lastDayOfMonth
+        //                                     }
+        // }     
         // );
-
-        // const query = [
-        //     {
-        //         '$match': {
-        //             'createdAt': {
-        //                 $gte: firstDayOfMonth,
-        //                 $lte: lastDayOfMonth
-        //             }
-        //         }
-        //     },
-        //     {
-        //         '$group': {
-        //             '_id' : null,
-        //             'sum' : { '$sum': "price" }
-        //         }
-        //     }
-        // ];
-        const sum_data = await DishModel.find({
-                                            createdAt: {
-                                                $gte: firstDayOfMonth,
-                                                $lte: lastDayOfMonth
-                                            }
-        }     
-        );
-        const total = await DishModel.aggregate([
-            {
-                $match: {
-                    createdAt: {
-                        $gte: firstDayOfMonth,
-                        $lte: lastDayOfMonth
-                    }
-                }
-            },
-            {
-                $group: {
-                    _id : null,
-                    sum : { $sum: "$price" }
-                }
-            }
-        ]).then(function(data){
-            console.log(data)
-        });
-
-        console.log(days_data, total);
-        res.status(200).json(days_data);
+        //const sum_data = await DishModel.find({ recipe:  /1/});
+        const sum_data = await BankModel.aggregate([
+            {$project: {name: 1, price: 1, create_month: {$month: '$createdAt'}}},
+            {$match: {create_month: month}},
+            {$group: {_id : null,sum : { $sum: "$proceeds" }}}
+        ])
+        const year_data=[];
+        for (let i = 1; i <= 12; i++){
+            const sum_data = await BankModel.aggregate([
+                {$project: {name: 1, price: 1, 
+                            create_month: {$month: '$createdAt'}, 
+                            create_year: {$year: '$createdAt'}
+                        }
+                },
+                {$match: {create_month: i, create_year: year}},
+                {$group: {_id : null,sum : { $sum: "$proceeds" }}}
+            ])
+            let month = {month: i};
+            //const month_data = Object.assign(month, sum_data);
+            year_data.push(sum_data)
+        }
+        res.status(200).json({sum_data: year_data});
     } catch (error) {
         res.status(500).json({ error: error });
     }
